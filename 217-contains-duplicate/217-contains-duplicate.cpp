6ccd41e0c07a090522d4572177107825e1daf3fc
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-01-07 14:02:40 +0530

    Time: 124 ms (17.21%), Space: 46.6 MB (42.56%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
    bool containsDuplicate(vector<int>& nums) 
     {
-        sort(nums.begin(), nums.end());
+        std::sort(nums.begin(), nums.end());
         
         for(int i = 0;i < nums.size() - 1; i++)
         {
