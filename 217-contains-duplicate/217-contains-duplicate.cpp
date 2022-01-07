0030f7e8395e1eadde16c5265abd4f5e0bab9715
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-01-07 13:51:28 +0530

    Time: 166 ms (7.46%), Space: 46.6 MB (34.92%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,12 +4,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int flag =0;
+        
         for(int i=0;i<=n-2;i++){
             if(nums[i]==nums[i+1]){
-                flag =1;
                 return true;
-                break;
             }
         }
     return false;
