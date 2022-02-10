3d39fb348adf0b4d5bda843f6fa834052d3c30da
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-02-10 23:31:24 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.40%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    int tb[38];
     int tribonacci(int n) {
-        vector<int> tb(n+1,0);
+       
         if(n<=1){
             return n;
         }
         if(n==2){
             return 1;
         }
-        tb[0] = 0;
-        tb[1] = 1;
-        tb[2] =1;
-        
-        for(int i=3;i<=n;i++){
-            tb[i] = tb[i-3] + tb[i-2] + tb[i-1];
+        if(tb[n]!=0){
+            return tb[n];
         }
-        return tb[n];
+        
+        
+        int trib = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        tb[n] = trib;
+        return trib;
     }
 };
