7749582a1ea2e03649970493cf2a9e8d1873f898
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-02-16 18:13:21 +0530

    Time: 43 ms (30.60%), Space: 31.1 MB (26.94%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+        int prefixS = 0;
+        for(int i=0;i<nums.size();i++){
+            total += nums[i];
+        }
+        
+        for(int i=0;i<nums.size();prefixS+=nums[i++]){
+            if(prefixS*2 == total-nums[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
