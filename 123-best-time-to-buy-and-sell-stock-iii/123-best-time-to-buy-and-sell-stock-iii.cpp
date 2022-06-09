a2b0a040fff7f441cb6dbfb645885d684fbb2fd0
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-09 16:00:18 +0530

    Time: 219 ms (62.71%), Space: 76.1 MB (69.83%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int lsf = prices[0];
+        int mpist = 0;
+        int dpl[n];
+        dpl[0] = 0;
+        for(int i=1;i<n;i++){
+            if(prices[i]<lsf){
+                lsf=prices[i];
+            }
+            mpist = prices[i] - lsf;
+            if(mpist>dpl[i-1]){
+                dpl[i]=mpist;
+            }else{
+                dpl[i] = dpl[i-1];
+            }
+        }
+        
+        int maxsf = prices[n-1];
+        int mpibt = 0;
+        int dpr[n];
+        dpr[n-1] = 0;
+        for(int i=n-2;i>=0;i--){
+            if(prices[i]>maxsf){
+                maxsf=prices[i];
+            }
+            mpibt = maxsf-prices[i];
+            if(mpibt>dpr[i+1]){
+                dpr[i] =mpibt; 
+            }else{
+                dpr[i] = dpr[i+1];
+            }
+        }
+        
+        int overallprofit = 0;
+        for(int i=0;i<n;i++){
+            if(overallprofit<dpr[i]+dpl[i]){
+                overallprofit = dpr[i]+dpl[i];
+            }
+        }
+        return overallprofit;
+    }
+};
