8eeaba45c209aa03ff1a10de60b39d194c87f492
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-10 18:54:44 +0530

    Time: 6 ms (72.42%), Space: 7.9 MB (73.91%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int countOne(int n){
+ /* TC- O(N)
+ int countOne(int n){
         int count = 0;
         while(n>0){
             int rem = n%2;
@@ -11,12 +12,25 @@ public:
         }
         return count;
     }
+
+*/
     vector<int> countBits(int n) {
-        vector<int> res;
+     /*   vector<int> res;
         for(int i=0;i<=n;i++){
             int a = countOne(i);
             res.push_back(a);
         }
         return res;
+    */
+        
+        vector<int> dp(n+1,0);
+     int offset = 1;
+        for(int i=1;i<n+1;i++){
+            if(offset*2 == i){
+                offset = i;
+            }
+            dp[i] = 1+dp[i-offset];
+        }
+        return dp;
     }
 };
