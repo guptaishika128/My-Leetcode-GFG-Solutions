de0f75e8fc0ccbe2195b05d028dc7832daba67a4
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-12 02:24:30 +0530

    Time: 3 ms (42.50%), Space: 5.8 MB (90.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int pathsCount(int m , int n,  vector<vector<int>>& dp){
+ /*   int pathsCount(int m , int n,  vector<vector<int>>& dp){
         if(m==0 && n==0){
             return 1;
         }
@@ -15,8 +15,18 @@ public:
         
         return dp[m][n] = up+left;
     }
+    */
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+       /* vector<vector<int>> dp(m,vector<int>(n,-1));
         return pathsCount(m-1,n-1,dp);
+      */
+        int totaldir = m+n-2;
+        int r = m-1;
+        double res = 1;
+        
+        for(int i=1;i<=r;i++){
+            res = res*(totaldir-r+i)/i;
+        }
+        return (int)res;
     }
 };
