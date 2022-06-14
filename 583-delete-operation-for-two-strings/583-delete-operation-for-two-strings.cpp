f4aaee280788b071f0ff832d7ba7d60749c91afd
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-14 16:53:05 +0530

    Time: 33 ms (43.72%), Space: 12.1 MB (54.28%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -7,9 +7,8 @@ public:
         
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                 if (i == 0 || j == 0){
-                     dp[i][j] =0 ;
-                 }
+                 if (i == 0 || j == 0)
+                     continue;
                 else{ 
                     dp[i][j] = (word1[i-1]==word2[j-1]) ? 1 + dp[i-1][j-1] : max(dp[i-1][j] , dp[i][j-1]);
             }
