c40dbe25aff90f99190ef5788cbc0471aaed6771
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-14 16:54:26 +0530

    Time: 53 ms (20.52%), Space: 12.4 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -5,13 +5,10 @@ public:
         int m = word2.size();
         vector<vector<int>> dp(n+1 ,vector<int>(m+1,0));
         
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                 if (i == 0 || j == 0)
-                     continue;
-                else{ 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                     dp[i][j] = (word1[i-1]==word2[j-1]) ? 1 + dp[i-1][j-1] : max(dp[i-1][j] , dp[i][j-1]);
-            }
+            
         }
     }
       return n+m-2*dp[n][m];        
