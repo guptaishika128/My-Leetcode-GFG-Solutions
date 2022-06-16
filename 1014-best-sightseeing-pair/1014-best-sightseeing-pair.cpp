d5894793a83682709b1d09baebdd183fa36ed4c2
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-16 16:13:40 +0530

    Time: 79 ms (45.09%), Space: 39.8 MB (23.25%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+         ios_base::sync_with_stdio(false);
+         cin.tie(0);cout.tie(0);
+	
+        if(values.size()<2){
+            return 0;
+        }
+        if(values.size()==2){
+            return (values[0]+values[1]-1);
+        }
+        
+        int currmax_score = values[0] + 0;
+        int res= -1;
+        
+        for(int i=1;i<values.size();i++){
+            res= max(res, currmax_score+ values[i]-i);
+            currmax_score = max(currmax_score, values[i]+i);
+        }
+        return res;
+    }
+};
