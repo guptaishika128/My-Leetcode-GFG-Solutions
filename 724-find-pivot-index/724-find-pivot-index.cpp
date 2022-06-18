55e683a7df9e6499083e1e3144d200ff2665997f
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-19 00:52:30 +0530

    Time: 42 ms (33.59%), Space: 31.2 MB (53.60%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -7,10 +7,21 @@ public:
             total += nums[i];
         }
         
-        for(int i=0;i<nums.size();prefixS+=nums[i++]){
+     /*   for(int i=0;i<nums.size();prefixS+=nums[i++]){
             if(prefixS*2 == total-nums[i]){
                 return i;
             }
+        }
+        return -1;
+        */
+        int left = 0;
+        int right = total;
+        for(int i=0;i<nums.size();i++){
+            right-=nums[i];
+            if(left==right){
+                return i;
+            }
+            left+=nums[i];
         }
         return -1;
     }
