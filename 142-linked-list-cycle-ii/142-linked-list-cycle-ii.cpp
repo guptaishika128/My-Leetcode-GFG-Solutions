bc2296e5765552a856a1c9ee95ae1277815d3916
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-21 11:44:26 +0530

    Time: 10 ms (69.31%), Space: 7.5 MB (73.24%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+     
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast= fast->next->next;
+            if(slow==fast){
+                break;
+            }
+        }
+        if(slow!=fast){
+            return NULL;
+        }
+        fast = head;
+        
+        while(fast && fast->next){
+            if(slow==fast){
+                return slow;
+            }
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return NULL;
+    }
+};
