8926fac9db5e880fceaf543399f6c81ba9f4a0b7
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-23 18:35:46 +0530

    Time: 40 ms (8.75%), Space: 8.1 MB (36.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int romanToInt(string s) {
-       int ans = 0, num = 0;
-        for (int i = s.size()-1; i>=0; i--) {
-            switch(s[i]) {
-                case 'I': num = 1; break;
-                case 'V': num = 5; break;
-                case 'X': num = 10; break;
-                case 'L': num = 50; break;
-                case 'C': num = 100; break;
-                case 'D': num = 500; break;
-                case 'M': num = 1000; break;
+        unordered_map<char,int> umap;
+        umap['I'] = 1;
+        umap['V'] = 5;
+        umap['X'] = 10;
+        umap['L'] = 50;
+        umap['C'] = 100;
+        umap['D'] = 500;
+        umap['M'] = 1000;
+        
+        int ans = 0;
+        for(int i=0;i<s.length();i++){
+            if(i+1<s.length() && (umap[s[i]] < umap[s[i+1]])){
+                ans -= umap[s[i]];
+            } else{
+                ans += umap[s[i]];
             }
-            if (4 * num < ans) ans -= num;
-            else ans += num;
         }
         return ans;
     }
