4ae2e6128259f200757f1ebc10dd90738ba11177
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-06-27 01:22:44 +0530

    Time: 74 ms (20.98%), Space: 12.1 MB (97.23%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
      void dfs(int i, int j ,vector<vector<char>>& grid){
-         // boundary checking
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
+         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0')
             return;
-        // return if current position is of water or is already visited
-        if(grid[i][j] == '2' || grid[i][j] == '0')
-            return;
-        
-        // mark the current as visited
-        grid[i][j] = '2';
-        
-        // do DFS in all 4 directions
-        dfs(i+1, j , grid);
-        dfs(i, j-1 , grid);
-        dfs(i-1, j , grid);
-        dfs(i, j+1 , grid);
-         
+        grid[i][j] = '0';
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i,j-1,grid);
     }
+    
     int numIslands(vector<vector<char>>& grid) {
-         int islands = 0;
-        // We make each 1 as 2 in when it is visited
-        for(int i = 0; i < grid.size(); i++) {
-            for(int j = 0; j < grid[0].size(); j++) {
-                // do DFS in case has not been visited and there is land
-                if(grid[i][j] == '1') {
+        int row = grid.size();
+        int col = grid[0].size();
+        int islands = 0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
+                    islands++;
                     dfs(i,j,grid);
-                    ++islands;
-                } 
+                }
             }
         }
-        return islands;     
+        return islands;    
     }
 };
