e3959af1d5e89bd964ff2936d975ae925f2134dc
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-07-05 01:24:55 +0530

    Time: 30 ms (10.02%), Space: 8.1 MB (57.05%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -12,7 +12,7 @@ public:
          if (!head)
             return false;
         
-        ListNode* slow = head, * fast = head->next;
+        ListNode* slow = head, * fast = head;
         
         while (fast && fast->next) {
             slow = slow->next;
