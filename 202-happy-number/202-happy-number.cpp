ca8404c82afd8a48e8d0c472d515443f0c49aa94
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-07-06 22:09:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (84.37%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
+    int digitSquareSum(int n){
+        int sum =0 , temp;
+        while(n){
+            temp=n%10;
+            sum += temp*temp;
+            n/=10;
+        }
+        return sum;
+    }
+    
+    
     bool isHappy(int n) {
-        unordered_map<int,int> mp;
+     /*   unordered_map<int,int> mp;
         
         while(n!=1){
             if(mp[n]==0){
@@ -17,5 +28,17 @@ public:
             n= sum;
     }
     return true;
+    */
+    int slow, fast;
+    slow = fast = n;
+    do {
+        slow = digitSquareSum(slow);
+        fast = digitSquareSum(fast);
+        fast = digitSquareSum(fast);
+    } while(slow != fast);
+    if (slow == 1) return 1;
+    else return 0;
+        
+        
     }
 };
