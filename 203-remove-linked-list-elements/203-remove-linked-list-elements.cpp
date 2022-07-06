2cb625502ec1e82d182f2a7cabd4a5bb6ef8783b
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-07-06 13:48:12 +0530

    Time: 42 ms (31.28%), Space: 15 MB (46.23%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -31,5 +31,21 @@ public:
             curr = curr->next;
         }
         return head;
+        
+        /*
+        while(cur){
+            
+            // If the next of cur node is to be deleted
+            // then point the next pointer of cur node to the next pointer
+            // of the node that has to be deleted i.e. the next of (next of cur)
+            if(cur->next && cur->next->val == val)
+                cur->next = cur->next->next;
+            
+            // If next of cur is save and should not be deleted then move cur pointer to it
+            // Because we want that till cur pointer we have the nodes that need not be deleted
+            else cur = cur->next;
+            
+        }
+        */
     }
 };
