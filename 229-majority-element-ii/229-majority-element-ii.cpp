4755ec3032169e1af486bda5eb2bc754ea3539a2
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-07-22 10:29:29 +0530

    Time: 12 ms (90.55%), Space: 15.9 MB (51.28%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> res;
-        unordered_map<int,int> mp;
+     //   vector<int> res; // for storing result
+    /*    unordered_map<int,int> mp; // map for storing frequency of vector elemnts
         
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
@@ -14,5 +14,45 @@ public:
             }
         }
         return res;
+        */
+        
+       int sz = nums.size();
+        int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+            else if (count1 == 0)
+            {
+                num1 = nums[i];
+                count1 = 1;
+            }    
+            else if (count2 == 0)
+            {
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> ans;
+        count1 = count2 = 0;
+        for (i = 0; i < sz; i++)
+        {
+            if (nums[i] == num1)
+                count1++;
+            else if (nums[i] == num2)
+                count2++;
+        }
+        if (count1 > sz/3)
+            ans.push_back(num1);
+        if (count2 > sz/3)
+            ans.push_back(num2);
+        return ans;
     }
 };
