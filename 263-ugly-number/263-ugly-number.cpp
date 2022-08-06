b47002bd6c14d57c066804607202cb96bb8584f2
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-08-06 14:42:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.58%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        int num = n;
+    if (num <= 0) return false;
+    while (num % 2 == 0) num /= 2;
+    while (num % 3 == 0) num /= 3;
+    while (num % 5 == 0) num /= 5;
+    return num == 1;
+    }
+};
