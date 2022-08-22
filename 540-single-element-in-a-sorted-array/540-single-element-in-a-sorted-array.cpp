7da079f2694e306ca572b329e5c299d5191e21f7
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2022-08-22 14:36:48 +0530

    Time: 38 ms (63.57%), Space: 22.2 MB (91.54%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+    /*   TC - O(N)  
         int xorr = 0;
         for(int i=0;i<nums.size();i++){
             xorr ^= nums[i];
         }
         return xorr;
+        */
+        // FOR TC - O(logN) -> BINARY SEARCH IS USED
+        int low = 0;
+        int high = nums.size()-2;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            } else{
+                if(nums[mid]==nums[mid+1]){
+                    high = mid-1;
+                } else{
+                    low = mid+1;
+                }
+            }
+        }
+        return nums[low];
     }
 };
