063c0117811f2f3830d78a46f6c89fc3b1b5c3e0
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-10 14:39:06 +0530

    Time: 5 ms (26.85%), Space: 8.5 MB (92.51%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+        int n = citations.size();
+        int low = 0, high = n-1;
+        
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(citations[mid] == (n-mid)){
+                return citations[mid];
+            } else if(citations[mid]>(n-mid)){
+                high = mid-1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        return n-low;
+    }
+};
