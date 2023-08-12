caad4728e830a8ada3d7fab4a480c51bde7ac4a8
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-12 17:48:58 +0530

    Time: 316 ms (29.41%), Space: 98.8 MB (12.11%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -8,15 +8,18 @@ public:
         while(j<nums.size()){
             mp[nums[j]]++;
             sum += nums[j];
-            if(j-i+1 == k){
+            if(j-i+1 <k){
+                j++;
+            }
+            else if(j-i+1 == k){
                 if (mp.size() == k) ans = max(ans,sum);
                 sum -= nums[i];
                 mp[nums[i]]--;
                 if(mp[nums[i]] == 0) mp.erase(nums[i]);
                 i++;
-               
+               j++;
             }
-            j++;
+            // j++;
         }
         return ans;
     }
