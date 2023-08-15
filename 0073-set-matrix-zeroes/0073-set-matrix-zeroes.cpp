923ecd1f791008718820a7d5e275e974419270c4
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-15 20:20:44 +0530

    Time: 12 ms (81.44%), Space: 13.3 MB (72.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,21 +6,16 @@ public:
         bool isRow = false;
         bool isCol = false;
         
-        for(int i = 0;i<m;i++){
-            if(matrix[i][0] == 0){
-                isRow = true;
-            }
-        }
         
-        for(int j=0;j<n;j++){
-            if(matrix[0][j] == 0){
-                isCol = true;
-            }
-        }
-        
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j] == 0){
+                    if(i==0){
+                        isCol = true;
+                    }
+                    if(j==0){
+                        isRow = true;
+                    }
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
