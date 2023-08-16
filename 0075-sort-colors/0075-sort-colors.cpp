71d16cc5a12ff212c4a64b21fb5ea0d7ac6b4b3e
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-16 13:17:51 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (82.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                count0++;
+        int n = nums.size();
+        // int count0 = 0;
+        // int count1 = 0;
+        // int count2 = 0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==0){
+        //         count0++;
+        //     }
+        //     else if(nums[i]==1){
+        //         count1++;
+        //     } else{
+        //         count2++;
+        //     }
+        // }
+        // for(int i=0;i<count0;i++){
+        //     nums[i] = 0;
+        // }
+        // for(int i=count0;i<count0 + count1;i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i=count0+count1;i<n;i++){
+        //     nums[i] = 2;
+        // }
+        
+        // Dutch National Flag Algo
+        
+        int low = 0;
+        int high = n-1;
+        int mid = 0;
+        
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low++],nums[mid++]);
             }
-            else if(nums[i]==1){
-                count1++;
+            else if(nums[mid] == 1){
+                mid++;
             } else{
-                count2++;
+                swap(nums[high--],nums[mid]);
             }
-        }
-        for(int i=0;i<count0;i++){
-            nums[i] = 0;
-        }
-        for(int i=count0;i<count0 + count1;i++){
-            nums[i] = 1;
-        }
-        for(int i=count0+count1;i<nums.size();i++){
-            nums[i] = 2;
         }
     }
 };
