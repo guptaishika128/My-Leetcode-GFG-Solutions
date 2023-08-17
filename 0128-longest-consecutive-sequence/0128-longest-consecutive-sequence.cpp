107c5e472484bc834eaff3483ae73387f6cee214
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-17 15:52:12 +0530

    Time: 130 ms (69.80%), Space: 67 MB (53.81%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        sort(nums.begin(),nums.end()); // Distorting the array 
+//         sort(nums.begin(),nums.end()); // Distorting the array 
         
         if(nums.size()<=1){
             return nums.size();
         }
         
-        int currentStreak = 1;
-        int maxi = 0;
+//         int currentStreak = 1;
+//         int maxi = 0;
         
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] != nums[i-1]){
-                if(nums[i] == (nums[i-1] + 1)){
-                    currentStreak++; 
-                } 
-                else{
-                     maxi = max(maxi , currentStreak);
-                    currentStreak = 1;
-                }
-            } 
+//         for(int i=1;i<nums.size();i++){
+//             if(nums[i] != nums[i-1]){
+//                 if(nums[i] == (nums[i-1] + 1)){
+//                     currentStreak++; 
+//                 } 
+//                 else{
+//                      maxi = max(maxi , currentStreak);
+//                     currentStreak = 1;
+//                 }
+//             } 
+//         }
+//         return max(maxi,currentStreak);
+        
+        int maxi = 1;
+        unordered_set<int> s;
+        
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
         }
-        return max(maxi,currentStreak);
+        
+        for(auto it:s){
+            if(s.find(it-1) == s.end()){
+                int curr =1;
+                int x = it;
+                while(s.find(x+1) != s.end()){
+                    curr++;
+                    x =x+1;
+                }
+                maxi = max(maxi,curr);
+            }
+        }
+        return maxi;
     }
 };
