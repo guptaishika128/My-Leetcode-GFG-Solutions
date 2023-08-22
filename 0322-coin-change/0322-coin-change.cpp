b6b16c78172b6f9ed7c80a09270afd8f0a136ab8
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-22 23:25:19 +0530

    Time: 109 ms (48.19%), Space: 45.1 MB (34.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -26,7 +26,33 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = solve(n-1,amount,coins,dp);
+        // int ans = solve(n-1,amount,coins,dp);
+        
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0] == 0){
+                dp[0][i] = i/coins[0]; 
+            }
+            else{
+                dp[0][i] = 1e9;
+            }
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int amt=0;amt<=amount;amt++){
+                int notTake = 0 + dp[ind-1][amt];
+        
+                int take = 1e9;
+
+                if(coins[ind]<=amt){
+                    take = 1 + dp[ind][amt-coins[ind]];
+                }
+
+            dp[ind][amt] = min(take,notTake);
+            }
+        }
+        
+        int ans = dp[n-1][amount];
+        
         if(ans>=1e9){
             return -1;
         }
