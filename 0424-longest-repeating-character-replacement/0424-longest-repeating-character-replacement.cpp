40776c94aa3005ac7a2c1c8c8e0de6e8a82e881d
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-23 23:14:32 +0530

    Time: 13 ms (51.34%), Space: 7.2 MB (16.37%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,42 +1,49 @@
 class Solution {
-private:
-    bool canMakeValidSubstring(string s, int substringlen , int k){
-        // takes a window of length substring_len on the given string and see if its valid
-        vector<int> freq(26,0);
-        int maxfreq = 0;
-        int start = 0;
+// private:
+//     bool canMakeValidSubstring(string s, int substringlen , int k){
+//         // takes a window of length substring_len on the given string and see if its valid
+//         vector<int> freq(26,0);
+//         int maxfreq = 0;
+//         int start = 0;
         
+//         for(int end = 0;end<s.length();end++){
+//             freq[s[end]-'A']++;
+            
+//             if(end-start+1>substringlen){
+//                 freq[s[start]-'A']--;
+//                 start++;
+//             }
+            
+//             maxfreq = max(maxfreq , freq[s[end]-'A']);
+            
+//             if(substringlen - maxfreq <= k){
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+//     }
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26,0);
+        int max_freq = 0;
+        int start = 0;
+        int result = 0;
         for(int end = 0;end<s.length();end++){
             freq[s[end]-'A']++;
             
-            if(end-start+1>substringlen){
+            max_freq = max(max_freq,freq[s[end]-'A']);
+            
+            while(end-start+1-max_freq>k){
                 freq[s[start]-'A']--;
                 start++;
+                
+                for(int i=0;i<26;i++){
+                    max_freq = max(max_freq, freq[i]);
+                }
             }
-            
-            maxfreq = max(maxfreq , freq[s[end]-'A']);
-            
-            if(substringlen - maxfreq <= k){
-                return true;
-            }
+            result = max(result,end-start+1);
         }
-        
-        return false;
-    }
-public:
-    int characterReplacement(string s, int k) {
-        int lo = 1;
-        int hi = s.length()+1;
-        
-        while(lo+1<hi){
-            int mid = (lo+hi)/2;
-            
-            if(canMakeValidSubstring(s,mid,k)){
-                lo = mid;
-            } else{
-                hi = mid;  
-            }
-        }
-        return lo;
+        return result;
     }
 };
