fe402830e1cd54e56458278b5bc7ffbf390d85a4
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-27 22:15:54 +0530

    Time: 69 ms (85.21%), Space: 31 MB (72.50%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int dp[2001][2001];
+     unordered_map<int,int> mp;
     bool solve(vector<int>& stones , int idx, int k){
         if(idx == stones.size()-1){
             return true;
@@ -18,14 +19,14 @@ public:
         for(int t=-1;t<2;t++){
             int next_jump = k+t;
             
-            if(next_jump>0){
+            if(next_jump>0 && mp.find(stones[idx]+next_jump)!=mp.end()){
                 
-                int in = lower_bound(stones.begin(),stones.end(),stones[idx] + next_jump) - stones.begin();
+//                 int in = lower_bound(stones.begin(),stones.end(),stones[idx] + next_jump) - stones.begin();
                 
-            if(in == stones.size() || stones[in] != stones[idx]+next_jump){
-                continue;
-            }
-             ans = ans || solve(stones,in,next_jump);   
+//             if(in == stones.size() || stones[in] != stones[idx]+next_jump){
+//                 continue;
+//             }
+             ans = ans || solve(stones,mp[stones[idx]+next_jump],next_jump);   
             }
         }
         
@@ -39,11 +40,11 @@ public:
         if(stones.size()==2){
             return (stones[1]-stones[0] == 1);
         }
-//         unordered_map<int,int> mp;
+       
         
-//          for (int i = 0; i < stones.size(); i++) {
-//             mp[stones[i]] = i;
-//         }
+         for (int i = 0; i < stones.size(); i++) {
+            mp[stones[i]] = i;
+        }
         
          memset(dp,-1,sizeof(dp));
         return solve(stones,0,0);
