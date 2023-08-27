d55caf18a6a2dd529ebb563a1cde3c02ccc377ea
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-27 22:05:27 +0530

    Time: 55 ms (92.01%), Space: 28.6 MB (77.64%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
     int dp[2001][2001];
-  bool find(vector<int> &stones, int i,int k){
-  	//we reached the ith stone with k jumps 
-  	
-      if(i==stones.size()-1) // if i== n-1 i.e. we have reached the last stone so we simply return true
-          return true;
-      if(i>=stones.size()) 
-          return false;
-  	
-      if(dp[i][k]!=-1) // if we already encountered this state ,we'll simply return it
-          return dp[i][k]; 
-  		
-      bool res = false;
-  	// from here we can either go to k-1 , k , k +1 th stone 
-      for(int j=-1;j<=1;j++){
-          int next_jump=k+j;
-          if(next_jump>0){
-  		
-  		//now the next stone would be = currstone + jump
-  		//using binary search to find the index of next stone if it exists
-           int in= lower_bound(stones.begin(),stones.end(),stones[i]+next_jump)-stones.begin();
-  			
-           if(in == stones.size() || stones[in]!=stones[i]+next_jump) // checking existence of next_stone in array
-                  continue;
-  			//if next stone exists then we will move to that index (in) with the jump that we took
-              res=res||find(stones,in,next_jump);
-          }
-      }
-  	
-      return dp[i][k]=res;
+    bool solve(vector<int>& stones , int idx, int k){
+        if(idx == stones.size()-1){
+            return true;
+        }
+        if(idx>=stones.size()){
+            return false;
+        }
+    
+        if(dp[idx][k]!=-1){
+            return dp[idx][k];
+        }
       
-  }
-  
-  bool canCross(vector<int>& stones) {
-     memset(dp,-1,sizeof(dp));
-     
-      return find(stones, 0,0);
-      
-  }
+        bool ans = false;
+        
+        for(int t=-1;t<2;t++){
+            int next_jump = k+t;
+            
+            if(next_jump>0){
+                
+                int in = lower_bound(stones.begin(),stones.end(),stones[idx] + next_jump) - stones.begin();
+                
+            if(in == stones.size() || stones[in] != stones[idx]+next_jump){
+                continue;
+            }
+             ans = ans || solve(stones,in,next_jump);   
+            }
+        }
+        
+        return dp[idx][k] = ans;
+       
+    }
+    bool canCross(vector<int>& stones) {
+        if(stones[1]-stones[0]>1){
+            return false;
+        }
+        if(stones.size()==2){
+            return (stones[1]-stones[0] == 1);
+        }
+//         unordered_map<int,int> mp;
+        
+//          for (int i = 0; i < stones.size(); i++) {
+//             mp[stones[i]] = i;
+//         }
+        
+         memset(dp,-1,sizeof(dp));
+        return solve(stones,0,0);
+    }
 };
