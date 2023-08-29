c9c7b37d8525f30e9a5b35cde6ac7c87e0f266c3
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-29 17:49:21 +0530

    Time: 3 ms (78.98%), Space: 6.8 MB (54.48%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.length();
+        int m = p.length();
+        
+         vector<vector<bool>> dp(m+1,vector<bool>(n+1));
+        
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = true;
+                }
+                else if(i == 0){
+                    dp[i][j] = false;
+                }
+                else if(j == 0){
+                   char pc = p[i-1];
+                    if(pc == '*')
+                        dp[i][j] = dp[i-2][j];
+                    else
+                        dp[i][j] = false;
+                }
+                else{
+                    char pc = p[i-1];
+                    char sc = s[j-1];
+                    
+                    if(pc == '*'){
+                        dp[i][j] = dp[i-2][j];
+                        
+                        char pslc = p[i-2];
+                        if(pslc == '.' || pslc == sc){
+                            dp[i][j] = dp[i][j-1] || dp[i-2][j];
+                        }
+                    } else if(pc == '.'){
+                        dp[i][j] = dp[i-1][j-1];
+                    } else if(pc == sc){
+                        dp[i][j] = dp[i-1][j-1];
+                    } else{
+                         dp[i][j] = false;
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
