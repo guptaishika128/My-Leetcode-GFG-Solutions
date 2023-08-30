5fb387f765114a0edcbd8345485d994414fe1505
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-30 23:35:10 +0530

    Time: 22 ms (83.51%), Space: 11.2 MB (52.63%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -2,33 +2,18 @@ class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n = s.length();
-        vector<int> ones(n); // To store number of 1s on the left side
-        vector<int> zeroes(n); // To store number of 0s on the right side
+        int count1 = 0;
+        int flips = 0;
         
-        if(s[0] == '1'){
-                ones[0] = 1;
-            } else{
-                ones[0] = 0;
-            }
-        
-        for(int i=1;i<n;i++){
+        for(int i=0;i<n;i++){
             if(s[i] == '1'){
-                ones[i] = ones[i-1] + 1;
-            } else{
-                ones[i] = ones[i-1];
+                count1++;
+            }
+            else{
+                flips++;
+                flips = min(flips,count1);
             }
         }
-    
-        
-        int mini_flips = INT_MAX;
-        
-        for(int i=0;i<=n;i++){
-            int curr_flips = (i == 0) ? 0 :ones[i-1];
-            curr_flips += (n-i)-(ones[n-1] - (i == 0 ? 0 : ones[i-1]));
-            
-            mini_flips = min(mini_flips , curr_flips);
-        }
-        
-        return mini_flips;
+        return flips;
     }
 };
