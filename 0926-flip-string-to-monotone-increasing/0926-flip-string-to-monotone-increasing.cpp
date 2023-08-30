c6d278b0dd0752bbb52e3faf5f85303f310cf215
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-30 23:32:53 +0530

    Time: 39 ms (35.69%), Space: 20 MB (23.91%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -18,26 +18,13 @@ public:
                 ones[i] = ones[i-1];
             }
         }
-        
-        if(s[n-1] == '0'){
-                zeroes[n-1] = 1;
-            } else{
-                zeroes[n-1] = 0;
-            }
-            
-        for(int i=n-2;i>=0;i--){
-            if(s[i] == '0'){
-                zeroes[i] = zeroes[i+1] + 1;
-            } else{
-                zeroes[i] = zeroes[i+1];
-            }
-        }
+    
         
         int mini_flips = INT_MAX;
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             int curr_flips = (i == 0) ? 0 :ones[i-1];
-            curr_flips += (i == n-1) ? 0 : zeroes[i];
+            curr_flips += (n-i)-(ones[n-1] - (i == 0 ? 0 : ones[i-1]));
             
             mini_flips = min(mini_flips , curr_flips);
         }
