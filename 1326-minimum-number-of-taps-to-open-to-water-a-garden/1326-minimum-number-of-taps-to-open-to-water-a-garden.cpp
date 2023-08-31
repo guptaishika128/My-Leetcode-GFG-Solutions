af64bfce89f1ad1d6024d1ed5680e92300a98f05
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-08-31 17:52:33 +0530

    Time: 45 ms (41.86%), Space: 14.1 MB (91.83%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -2,28 +2,25 @@ class Solution {
 public:
     
     int minTaps(int n, vector<int>& ranges) {
-        int min   = 0;
-        int max   = 0;
-        int count = 0;
-        while(max < n) {
+       int minReach = 0;
+       int maxReach = 0 ;
+        int ans =0;
+        
+        while(maxReach<n){
             
-            //Choose the tap with maximum range it can reach to right
-            //For a given min value (to left)
-            for(int i = 0; i < n + 1; i++) {
-                if(i-ranges[i] <= min && i+ranges[i] > max)
-                    max = i+ranges[i];
+            for(int i=0;i<n+1;i++){
+                if(i-ranges[i]<=minReach && i+ranges[i]>maxReach){
+                    maxReach = i +ranges[i];
+                }
             }
             
-            //It means we couldn't expand our range to right
-            if(max == min)
+            if(minReach == maxReach){
                 return -1;
+            }
             
-            //Now, we have reached till max, next we want to cover more than this max
-            //starting from min
-            min = max;
-            count++;
+            minReach = maxReach;
+            ans++;
         }
-        
-        return count;
+        return ans;
     }
 };
