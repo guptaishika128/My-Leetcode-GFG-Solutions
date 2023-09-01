a7de4977fb1785d8d43517ee337ce41db966c5c4
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-01 15:56:48 +0530

    Time: 3 ms (89.82%), Space: 7.7 MB (89.73%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -15,22 +15,19 @@ public:
 
 
     vector<int> countBits(int n) {
-       vector<int> res;
-        for(int i=0;i<=n;i++){
-            int a = countOne(i);
-            res.push_back(a);
-        }
-        return res;
+       // vector<int> res;
+       //  for(int i=0;i<=n;i++){
+       //      int a = countOne(i);
+       //      res.push_back(a);
+       //  }
+       //  return res;
     
         
-     //    vector<int> dp(n+1,0);
-     // int offset = 1;
-     //    for(int i=1;i<n+1;i++){
-     //        if(offset*2 == i){
-     //            offset = i;
-     //        }
-     //        dp[i] = 1+dp[i-offset];
-     //    }
-     //    return dp;
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        for(int i=1;i<n+1;i++){
+            dp[i] = dp[i>>1] + (i&1);
+        }
+        return dp;
     }
 };
