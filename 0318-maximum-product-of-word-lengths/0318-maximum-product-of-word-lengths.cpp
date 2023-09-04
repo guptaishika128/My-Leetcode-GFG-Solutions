81e5dcc4cf1e80880680c881d27a96e306cfb239
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-04 12:14:28 +0530

    Time: 136 ms (31.16%), Space: 21.8 MB (54.11%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    bool checkCommon(bitset<26>& a, bitset<26>& b){
+        for(int i=0;i<26;i++){
+            if(a[i] && b[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    int maxProduct(vector<string>& words) {
+        int n = words.size();
+        int ans = 0;
+        vector<bitset<26>> chars(n);
+        
+        for(int i=0;i<n;i++){
+            for(auto& ch: words[i]){
+                chars[i][ch-'a'] = 1;
+            }
+            
+            for(int j=0;j<i;j++){
+                if(!checkCommon(chars[i],chars[j])){
+                   ans = max(ans, int((words[i].size()) * (words[j].size())));
+                }
+            }
+        }
+        return ans;
+    }
+};
