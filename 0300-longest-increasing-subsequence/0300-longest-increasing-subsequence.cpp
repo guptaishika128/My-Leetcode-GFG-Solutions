dcc0271f3c39c0cef6a14a45e39a6a5f686d66d2
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-08 23:27:07 +0530

    Time: 233 ms (69.68%), Space: 10.7 MB (42.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,20 +5,17 @@ public:
         if(n == 1){
             return 1;
         }
-        vector<int> dp(n);
+        vector<int> dp(n,1);
         // dp[i] -> stores length of lis that ends that nums[i]
-        dp[0] = 1; //  Base case
+
         int ans = 0;
         for(int i=1;i<n;i++){
             int curr_max = 0;
             for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    if(curr_max<dp[j]){
-                        curr_max = dp[j];
-                    }
+                if(nums[j]<nums[i] && dp[i]<dp[j]+1){
+                   dp[i] = dp[j]+1;
                 }
             }
-            dp[i] = curr_max + 1;
             
             if(ans<dp[i]){
                 ans = dp[i];
