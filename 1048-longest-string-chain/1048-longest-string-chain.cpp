8fc86a81a9ccdb76448ed89f4d2744a6fd4d72a3
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-08 23:27:31 +0530

    Time: 82 ms (77.36%), Space: 13.2 MB (72.39%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -35,7 +35,7 @@ public:
        
        int maxi = 1;
         
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(isPossible(words[i],words[j]) && dp[i] < dp[j] +1){
                     dp[i] = dp[j]+1;
