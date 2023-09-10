0ffd0e7d769ed899c36178e7b2cc4e29679e0e7a
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-10 15:12:48 +0530

    Time: 2 ms (56.67%), Space: 6.1 MB (43.33%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     const int MOD = 1e9 + 7;
     int countOrders(int n) {
-       long long ans = 1; 
-        for(int i=2;i<=n;i++){
-            int space = 2*(i-1) + 1;
-            ans *= space*(space+1)/2;
-            ans = ans%MOD;
+       long long answer = 1; 
+         for(int i = 2; i <= n; i++){
+            answer = (answer * i * (2 * i - 1))%MOD;
         }
-        
-        return ans;
+
+        return answer;
     }
 };
