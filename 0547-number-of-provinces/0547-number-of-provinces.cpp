e0db8a764b1d40754eca74d158e7568a20a181af
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-22 03:20:46 +0530

    Time: 19 ms (82.74%), Space: 15.7 MB (66.02%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,3 +1,59 @@
+class DisjointSet{
+ 
+public:
+    vector<int> parent,rank,size;
+    
+    DisjointSet(int n){
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        rank.resize(n+1,0);
+        
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+    }
+    
+    int findUPar(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    void unionByRank(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(rank[ulp_u]>rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        } else if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        } else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+    
+    void unionBySize(int u, int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+
+};
+
 class Solution {
 private: 
     
@@ -15,26 +71,49 @@ public:
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> adj[n];
         
-       vector<bool> visited(n, false);
+        DisjointSet ds(n);
         
-        int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j] == 1 && i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                if(isConnected[i][j] == 1){
+                    ds.unionByRank(i,j);
                 }
             }
         }
         
+        int cnt = 0;
+        
         for(int i=0;i<n;i++){
-            if(!visited[i]){
+            if(ds.parent[i] == i){
                 cnt++;
-                dfs(i,visited,adj);
             }
-        }
+        } 
         return cnt;
+        
+        
+        
+        
+//         vector<int> adj[n];
+        
+//        vector<bool> visited(n, false);
+        
+//         int cnt = 0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(isConnected[i][j] == 1 && i!=j){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 cnt++;
+//                 dfs(i,visited,adj);
+//             }
+//         }
+//         return cnt;
     }
 };
