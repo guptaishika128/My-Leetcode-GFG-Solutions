446a385969cac831085da497d0a2635c5e4c77ad
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-23 01:06:33 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (47.78%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    void printSubsets(vector<int>& nums, int index , vector<int>& ds,vector<vector<int>>& result){
+    void printSubsets(vector<int>& nums, int index , vector<int>& ds,vector<vector<int>>& result,set<vector<int>>& s){
         if(index>=nums.size()){
+  
+            if(s.find(ds)==s.end()){
+            s.insert(ds);
             result.push_back(ds);
+            }
             return;
         }
         
         ds.push_back(nums[index]);
-        printSubsets(nums,index+1,ds,result);
+        printSubsets(nums,index+1,ds,result,s);
         ds.pop_back();
         
-        printSubsets(nums,index+1,ds,result);
-        /*
-         if(index>=nums.size()){
-        result.push_back(ds);
-        return;
-    }
-    
-    // If pick
-    ds.push_back(nums[index]);
-    printSubsets(nums,index+1,ds,result);
-    ds.pop_back();
-    
-    //If not pick
-    printSubsets(nums,index+1,ds,result);
-    */
+        printSubsets(nums,index+1,ds,result,s);
+      
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
         vector<int> ds;
-        printSubsets(nums,0,ds,result);
+        set<vector<int>> s;
+        printSubsets(nums,0,ds,result,s);
         return result;
     }
 };
