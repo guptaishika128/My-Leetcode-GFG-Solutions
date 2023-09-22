610163a4c4d5f226d8b3dbcd16cc6137ce078c00
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-22 19:20:19 +0530

    Time: 18 ms (86.31%), Space: 7 MB (10.20%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool isSafe(vector<vector<char>>& board, int row, int col, char c){
+        for(int i=0;i<9;i++){
+            if(board[i][col] == c){
+                return false;
+            }
+            if(board[row][i] == c){
+                return false;
+            }
+            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)] == c){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    
+    
+    bool solveSudoku(vector<vector<char>> &board, int i, int j)
+{
+    if(i==9) return true;
+    if(j==9) return solveSudoku(board, i+1, 0);
+    if(board[i][j] != '.') return solveSudoku(board, i, j+1);
+
+    for(char c='1'; c<='9'; c++)
+    {
+        if(isSafe(board, i, j, c))
+        {
+            board[i][j] = c;
+            if(solveSudoku(board, i, j+1)) return true;
+            board[i][j] = '.';
+        }
+    }
+        return false;
+    }
+    void solveSudoku(vector<vector<char>>& board) {
+       solveSudoku(board,0,0);
+    }
+};
