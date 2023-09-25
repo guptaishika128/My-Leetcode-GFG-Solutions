9d16fcd3e6bc99f2dc44814a2a2565c8d27674b7
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-25 22:42:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (10.88%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,10 +1,15 @@
+std::vector<int> dp(21, -1);
 class Solution {
 public:
+
     int numTrees(int n) {
         if(n <= 1) return 1;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
         int ans = 0;
         for(int i = 1; i <= n; i++) 
             ans += numTrees(i-1) * numTrees(n-i);
-        return ans;
+        return dp[n] = ans;
     }
 };
