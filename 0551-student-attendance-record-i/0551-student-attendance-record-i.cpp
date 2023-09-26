6534ee2021e12d8c3ad800fa420d8714fdf71dfe
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-09-26 12:10:02 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (13.41%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -1,27 +1,13 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int maxi = 'P'-'A';
-        bool flag = false;
-        vector<int> cnt(maxi+1,0);
-        int cnt1 = 0;
-        int max_freq = 0;
-        for(int i=0;i<s.length();i++){
-            cnt[s[i]-'A']++;
-            if(s[i]=='L'){
-                cnt1++;
-                max_freq = max(max_freq,cnt1);
-            }
-            else{
-                cnt1 = 0;
-            }
-        }
-        if(cnt[0]<2 && max_freq<3){
-            flag = true;
-        }
-        else{
-            flag = false;
-        }
-        return flag;
+         int a=0, l=0;
+    for(int i=0;i<s.size();i++) {
+        if(s[i]=='A') a++;
+        if(s[i]=='L') l++;
+        else l=0;
+        if(a>=2||l>2) return false;
+    }
+    return true;
     }
 };
