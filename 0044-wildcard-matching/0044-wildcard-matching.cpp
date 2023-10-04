a3900caadc840773ade58d581f79c2e7283b5fab
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-10-04 22:43:55 +0530

    Time: 33 ms (84.24%), Space: 27.2 MB (51.37%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    
-    bool solve(int i , int j ,string& s , string& p , vector<vector<int>>& dp){
+    bool solve(int i, int j, string& s, string& p, vector<vector<int>>& dp){
         if(i<0 && j<0){
             return true;
         }
+        
         if(i<0 && j>=0){
             return false;
         }
-        
-       
         
         if(i>=0 && j<0){
             for(int i1=0;i1<=i;i1++){
@@ -19,22 +17,25 @@ public:
             }
             return true;
         }
-         if(dp[i][j] != -1){
+        
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if (p[i] == s[j] || p[i] == '?'){
+        
+        if(p[i] == s[j] || p[i] == '?'){
             return dp[i][j] = solve(i-1,j-1,s,p,dp);
         }
-        if(p[i] == '*'){
-            return dp[i][j] = solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
+        else if(p[i]=='*'){
+            return dp[i][j] = solve(i,j-1,s,p,dp) || solve(i-1,j,s,p,dp); 
         }
         return dp[i][j] = false;
     }
     
+    
     bool isMatch(string s, string p) {
-      int n = s.length();
-      int m = p.length();
-      vector<vector<int>> dp(m,vector<int>(n,-1));
-    return solve(m-1,n-1,s,p,dp);
+        int n = s.length();
+        int m = p.length();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,s,p,dp);
     }
 };
