e697be7bd95991144be125bad3a3dc9ec6d26435
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-10-04 23:03:29 +0530

    Time: 3 ms (68.24%), Space: 8 MB (26.02%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -2,24 +2,50 @@ class Solution {
 public:
     int countSubstrings(string s, string t) {
         int ans = 0;
-        for(int i=0;i<s.size();i++){
-            for(int j=0;j<t.size();j++){
-                int x = i;
-                int y = j;
-                int temp = 0;
-                while( x<s.size() && y<t.size() ){
-                    if(s[x]!=t[y]){
-                        temp++;
-                    }
-                    if(temp==1){
-                        ans += 1;
-                    }
-                    if(temp>=2){
-                        break;
-                    }
-                    x++;
-                    y++;
+        // for(int i=0;i<s.size();i++){
+        //     for(int j=0;j<t.size();j++){
+        //         int x = i;
+        //         int y = j;
+        //         int temp = 0;
+        //         while( x<s.size() && y<t.size() ){
+        //             if(s[x]!=t[y]){
+        //                 temp++;
+        //             }
+        //             if(temp==1){
+        //                 ans += 1;
+        //             }
+        //             if(temp>=2){
+        //                 break;
+        //             }
+        //             x++;
+        //             y++;
+        //         }
+        //     }
+        // }
+        // return ans;
+        int n = s.length();
+        int m = t.length();
+        
+        vector<vector<int>> sameStr(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    sameStr[i][j] = 1 + sameStr[i-1][j-1];
                 }
+            }
+        }
+        
+        vector<vector<int>> diffStr(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] != t[j-1]){
+                    diffStr[i][j] = 1 + sameStr[i-1][j-1];
+                }else{
+                    diffStr[i][j] = diffStr[i-1][j-1];
+                }
+                ans += diffStr[i][j];
             }
         }
         return ans;
