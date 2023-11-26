ee5fff27db2a87eb3d1f08129bfacf04380bf59b
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-11-26 07:22:08 +0530

    Time: 3 ms (92.79%), Space: 11.3 MB (33.77%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
-        if (i == j-1) {
-            res.push_back(num);
-            return;
-        }
-        for (int k = i; k < j; k++) {
-            if (i != k && num[i] == num[k]) continue;
-            swap(num[i], num[k]);
-            recursion(num, i+1, j, res);
-        }
-    }
-    vector<vector<int> > permuteUnique(vector<int> &num) {
-        sort(num.begin(), num.end());
-        vector<vector<int> >res;
-        recursion(num, 0, num.size(), res);
+     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        if(nums.size() == 0) return vector<vector<int>>() ;
+        //no need to sort nums to save time
+        vector<vector<int>> res ;
+        dfs(nums,0,res) ;
         return res;
+    }
+    void dfs(vector<int>& nums, int index , vector<vector<int>>& res)
+    {
+        if(index == nums.size() ) 
+        {
+            res.push_back(nums);
+            return ;
+        }
+        unordered_set<int> record ;
+        for(int i = index ; i < nums.size() ; i++)
+        {
+            if(record.find(nums[i]) != record.end() )
+                continue ;
+            record.insert(nums[i]);
+            swap(nums[i] , nums[index]);
+            dfs(nums,index+1,res);
+            swap(nums[i] , nums[index]);
+        }
     }
 };
