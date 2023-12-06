c646c1ebd3e228407a48f8dc4dc8a6a23d2deb22
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-12-06 14:40:22 +0530

    Time: 5 ms (30.46%), Space: 9.3 MB (61.86%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxi = *max_element(candies.begin(),candies.end());
+        vector<bool> ans;
+        for(int i=0;i<candies.size();i++){
+            if(maxi<=candies[i]+extraCandies){
+                ans.push_back(true);
+            }
+            else{
+                ans.push_back(false);
+            }
+        }
+        return ans;
+    }
+};
