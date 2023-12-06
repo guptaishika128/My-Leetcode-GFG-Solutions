e049f04da0a875ac86a88af1acfd48c7717fa9a8
Ishika Gupta <78978568+guptaishika128@users.noreply.github.com>
2023-12-06 15:43:18 +0530

    Time: 3 ms (97.08%), Space: 8.3 MB (26.01%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        vector<char> v;
+//         vector<char> v;
         
-        for(int i=0;i<s.length();i++){
-            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'|| s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
-                v.push_back(s[i]);
+//         for(int i=0;i<s.length();i++){
+//             if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'|| s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+//                 v.push_back(s[i]);
+//             }
+//         }
+        
+//         reverse(v.begin(),v.end());
+        
+//         int k = 0;
+//         for(int i=0;i<s.length();i++){
+//              if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'|| s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+//                  s[i] = v[k++];
+//             }
+//         }
+//         return s;
+        unordered_set<char> vowel = {'a','e','i','o','u','A','E','I','O','U'};
+        
+        int n = s.length();
+        
+        int i=0, j=n-1;
+        while(i<j){
+            while(i<j && vowel.find(s[i])==vowel.end()){
+                i++;
             }
-        }
-        
-        reverse(v.begin(),v.end());
-        
-        int k = 0;
-        for(int i=0;i<s.length();i++){
-             if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u'|| s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
-                 s[i] = v[k++];
+            while(i<j && vowel.find(s[j])==vowel.end()){
+                j--;
             }
+            if(i<j){
+                swap(s[i],s[j]);
+            }
+            i++;
+            j--;
         }
         return s;
+        
+        
     }
 };
